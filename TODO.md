75d7cd2ad9df7a41297515d09df21a268be12149
Naomi Hyldmar <42452088+nhyldmar@users.noreply.github.com>
2025-09-28 22:37:27 +0100

    Add door unlocking logic

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
-# TODO
+# TODO Checklist
 
 ## 1.0 Features
 
--- src/archipelago/ItemManager.cs
+++ src/archipelago/ItemManager.cs
@@ -4,6 +4,7 @@ using FezEngine.Services.Scripting;
 using FezEngine.Structure;
 using FezEngine.Tools;
 using FezGame.Services;
+using FezGame.Structure;
 using FEZUG.Features.Console;
 
 namespace FEZAP.Archipelago
@@ -18,6 +19,9 @@ namespace FEZAP.Archipelago
 
         [ServiceDependency]
         public IPlayerManager PlayerManager { private get; set; }
+
+        [ServiceDependency]
+        public ILevelManager LevelManager { private get; set; }
 
         [ServiceDependency]
         public IGameService GameService { private get; set; }
@@ -113,8 +117,29 @@ namespace FEZAP.Archipelago
                 case "Sunglasses":
                     GameState.SaveData.HasFPView = true;
                     break;
-                case "... Door Unlocked":
-                    // TODO: Add door unlock logic for each of the 8 doors
+                case "Boileroom Unlocked":
+                    UnlockDoor("VILLAGEVILLE_3D", new(35, 30, 36));
+                    break;
+                case "Lighthouse Door Unlocked":
+                    UnlockDoor("LIGHTHOUSE", new(21, 20, 27));
+                    break;
+                case "Tree Door Unlocked":
+                    UnlockDoor("TREE", new(41, 50, 2));
+                    break;
+                case "Well Door Unlocked":
+                    UnlockDoor("RAILS", new(14, 21, 14));
+                    break;
+                case "Windmill Door Unlocked":
+                    UnlockDoor("PIVOT_ONE", new(26, 61, 30));
+                    break;
+                case "Mausoleum Door Unlocked":
+                    UnlockDoor("MAUSOLEUM", new(21, 13, 23));
+                    break;
+                case "Sewer Hub Door Unlocked":
+                    UnlockDoor("SEWER_HUB", new(10, 42, 9));
+                    break;
+                case "Sewer Pillars Door Unlocked":
+                    UnlockDoor("SEWER_PILLARS", new(8, 14, 30));
                     break;
                 case "Rotation Trap":
                     DoRotationTrap();
@@ -128,6 +153,23 @@ namespace FEZAP.Archipelago
                 default:
                     FezugConsole.Print($"Unknown item: {item.ItemDisplayName}", FezugConsole.OutputType.Error);
                     break;
+            }
+        }
+
+        private void UnlockDoor(string levelName, TrileEmplacement trileEmplacement)
+        {
+            if (!GameState.SaveData.World.ContainsKey(levelName))
+            {
+                GameState.SaveData.World.Add(levelName, new LevelSaveData());
+            }
+
+            LevelSaveData levelData = GameState.SaveData.World[levelName];
+            levelData.InactiveTriles.Add(trileEmplacement);
+
+            // NOTE: Ideally we would reload the level
+            if (LevelManager.Name == levelName)
+            {
+                FezugConsole.Print("Re-enter this level to unlock the door");
             }
         }
 
