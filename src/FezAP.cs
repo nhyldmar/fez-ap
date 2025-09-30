87952fadb61236299fef4b0db47679bfb73cb421
Naomi Hyldmar <42452088+nhyldmar@users.noreply.github.com>
2025-09-30 18:24:02 +0100

    v0.1.1

--- src/FezAP.cs
+++ src/FezAP.cs
@@ -14,7 +14,7 @@ namespace FEZAP
 
     public class Fezap : DrawableGameComponent
     {
-        public static string Version = "v0.1.0";
+        public static string Version = "v0.1.1";
         public readonly Fezug Fezug = new();
         public static readonly ArchipelagoManager archipelagoManager = new();
         public static readonly DeathManager deathManager = new();
