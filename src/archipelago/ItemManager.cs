f4cb4431268655495f90b686ad1b7ff775f674bd
Terry Hearst <thearst3rd@gmail.com>
2026-09-13 14:57:49 -0400

    Don't rotate in first person mode

--- src/archipelago/ItemManager.cs
+++ src/archipelago/ItemManager.cs
@@ -310,6 +310,8 @@ namespace FEZAP.Archipelago
         {
             if (LevelManager.Flat)
                 return;
+            if (GameState.InFpsMode)
+                return;
             List<int> rotationOptions = [-2, -1, 1, 2];
             int index = RandomHelper.Random.Next(rotationOptions.Count);
             CameraService.Rotate(rotationOptions[index]);
